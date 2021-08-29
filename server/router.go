40d2a708b315e308e30969b58511a0691f554205
PatrickKvartsaniy <kvartsaniy@gmail.com>
2021-08-29 20:13:47 +0300

    update routes

--- server/router.go
+++ server/router.go
@@ -2,6 +2,21 @@ package server
 
 import "github.com/gorilla/mux"
 
+const (
+	telegramEndpoint       = "/bots/telegram/"
+	telegramNewBotEndpoint = telegramEndpoint + "new/"
+	telegramSendMessage    = telegramEndpoint + "send/"
+
+	viberEndpoint       = "/bots/viber/"
+	viberNewBotEndpoint = viberEndpoint + "new/"
+	viberSendMessage    = viberEndpoint + "/send"
+
+	signInTenantEndpoint = "/tenants/new"
+	signInUserEndpoint   = "/users/new"
+
+	loginEndpoint = "/login"
+)
+
 func (s *Server) router() *mux.Router {
 	router := mux.NewRouter()
 
@@ -12,6 +27,9 @@ func (s *Server) router() *mux.Router {
 	router.HandleFunc(viberEndpoint+"{bot}", s.webhook)
 	router.HandleFunc(viberNewBotEndpoint, s.newBot)
 	router.HandleFunc(viberSendMessage+"{bot}", s.send)
+
+	router.HandleFunc(signInTenantEndpoint, s.newTenant)
+	router.HandleFunc(signInUserEndpoint, s.newUser)
 
 	return router
 }
